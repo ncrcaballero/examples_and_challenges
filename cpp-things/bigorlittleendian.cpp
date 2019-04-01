92ebae9412914076dc2d7076b5e65fe792bf7d6a
ncrcaballero <30745998+ncrcaballero@users.noreply.github.com>
2019-04-01 18:15:28 -0300

    Update bigorlittleendian.cpp

--- cpp-things/bigorlittleendian.cpp
+++ cpp-things/bigorlittleendian.cpp
@@ -1,4 +1,12 @@
 // https://helloacm.com/how-to-find-out-whether-a-machine-is-big-endian-or-little-endian-in-cc/
+/*
+First, we declare a 16-bit integer (short int),
+which has the value 0x0001, 
+then we gets its pointer and deference it. 
+If the MSB is stored at lower address 
+(e.g. the value that pointer points to), 
+then it is little endian.
+*/
 #include <stdio.h>
  
 #define BIG_ENDIAN 0
